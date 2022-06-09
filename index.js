21751330414c947877ae1f83f572a371b8f37275
Wycliffe Ndiba <w.ndiba@gmail.com>
2022-06-09 07:58:56 +0300

    Done.

--- index.js
+++ index.js
@@ -1,32 +1,6 @@
 // Write your solution here!
 const cats = ["Milo", "Otis", "Garfield"]
 
-// Array functions
-//       destructivelyAppendCat(name)
-//         1) appends a cat to the end of the cats array
-
-//       destructivelyPrependCat(name)
-//         2) prepends a cat to the beginning of the cats array
-
-//       destructivelyRemoveLastCat()
-//         3) removes the last cat from the cats array
-
-//       destructivelyRemoveFirstCat()
-//         4) removes the first cat from the cats array
-
-//       appendCat(name)
-//         5) appends a cat to the cats array and returns a new array, leaving the cats array unchanged
-
-//       prependCat(name)
-//         6) prepends a cat to the cats array and returns a new array, leaving the cats array unchanged
-
-//       removeLastCat()
-//         7) removes the last cat in the cats array and returns a new array, leaving the cats array unchanged
-
-//       removeFirstCat()
-//         8) removes the first cat from the cats array and returns a new array, leaving the cats array unchanged
-
-
 
 const destructivelyAppendCat = (name) => {
     cats.push(name)
